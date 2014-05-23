34a2db97a965dceac0d980c9fa2ee0c3a5e0d04a
brunabarbosa <bruna.barbosa@ccc.ufcg.edu>
2014-05-23 16:52:21 -0300

    up

--- dinamica/jquery.js
+++ dinamica/jquery.js
@@ -3,15 +3,17 @@ $(document).ready(function() {
 	$('#btn-inserir').click(function(){
 			$('#select-from').append("<option value='"+$(interesse).val()+"'>"+$(interesse).val()+"</option>");
 	});
+	
     $('#btn-add').click(function(){
         $('#select-from option:selected').each( function() {
             $('#select-to').append("<option value='"+$(this).val()+"'>"+$(this).text()+"</option>");
             $(this).remove();
         });
     });
+    
     $('#btn-remove').click(function(){
         $('#select-to option:selected').each( function() {
-           $('#select-from').append("<option value='"+$(this).val()+"'>"+$(this).text()+"</option>");
+          // $('#select-from').append("<option value='"+$(this).val()+"'>"+$(this).text()+"</option>");
             $(this).remove();
         });
     });
